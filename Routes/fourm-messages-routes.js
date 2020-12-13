dbe75cee82c125c2d4aabb313567c20688098eee
Ori <wiegner.ori@gmail.com>
2020-12-13 12:26:59 -0500

    added routes

---
+++ Routes/fourm-messages-routes.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const shortid = require('shortid');
+const router = express.Router();
+
+router.use(express.json());
+
+const messages = [
+    {
+        "title": "IMO best movie",
+        "content": "The Shawshank Redemption",
+        "id": "1"
+    },
+    {
+        "title": "^^100% true",
+        "content": "I agree with your post",
+        "id": "2"
+    },
+    {
+        "title": "Defs not true",
+        "content": "No way the best movie is Super Mario Bros the movie",
+        "id": "3"
+    }
+];
+
+/**
+ * This allows users get all the messages of a specific topics
+ */
+router.get('', (req, res) => {
+    console.log('get requset from api/message has been made');
+    res.json(messages);
+});
+
+router.post('', (req, res) => {
+    const message = req.body
+    message.id = shortid.generate();
+    messages.push(message);
+    res.status(201).json(message);
+    console.log('post requset from api/fourms has been made :\n\t' + JSON.stringify(message));
+});
+
+
+/**
+ * This allows users delete a specific fourm message from the server
+ */
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    console.log("YYZ " + id);
+    for(i=0 ; i < messages.length ; i++){
+       if(messages[i].id == id){
+        res.status(201).json(messages[i]);
+        messages.splice(i , 1);
+        return;
+       }
+    }
+    res.status(500).json({messages : `delete requset from has NOT been made`});
+});
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -7,17 +7,22 @@ const server = express();
  */
 server.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header("Access-Control-Allow-Headers", "Origin, x-requested-with, content-type, accept, authorization")
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, x-requested-with, content-type, accept, authorization");
     next();
   });
 
 //Sets up the routes
 const fourmsRouter = require('../Routes/fourms-routes');
+const fourmMessageRouter = require('../Routes/fourm-messages-routes')
 
 const fourms = [];
 
 server.use(express.json());
 server.use('/api/fourms', fourmsRouter);
+server.use('/api/messages', fourmMessageRouter);
+
 
 server.get('', (req, res) => {
     res.json({ message: "*** This is the front page of the MyBoard server ***"})
