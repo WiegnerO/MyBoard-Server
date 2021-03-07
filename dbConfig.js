30a035658df0f35795c4a8da7e356a94bfae329e
Ori <wiegner.ori@gmail.com>
2021-03-07 14:38:14 -0500

    added connection

--- dbConfig.js
+++ dbConfig.js
@@ -1,4 +1,23 @@
 const dbEngine = process.env.DB_ENVIRONMENT || "development";
 const config = require("./knexfile")[dbEngine];
 
+const { Client } = require('pg');
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+    }
+    client.end();
+});
+
 module.exports = require("knex")(config);
